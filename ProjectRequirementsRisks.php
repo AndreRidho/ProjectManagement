a127f1c96da62a0da9cab20195dfbc372f96854d
MarkSuckmyberg <78968537+MarkSuckmyberg@users.noreply.github.com>
2021-07-04 18:00:51 +0800

    Add files via upload

--- ProjectRequirementsRisks.php
+++ ProjectRequirementsRisks.php
@@ -1,15 +1,82 @@
+<?php
+
+session_start();
+if(!$_SESSION["loggedin"]){
+  $_SESSION["message"] = "Please login first";
+  header("Location: login.php");
+  exit();
+}
+
+require_once 'pdo.php';
+$stm = $pdo->prepare("select * from question where section_num = 7");
+$stm->execute();
+array_push($_SESSION["totalWeight"], 0);
+$_SESSION["totalWeight"][6] = 0;
+
+if(isset($_POST["submit"])){
+
+  for($i=50 ; $i<=64 ; $i++){
+
+    if(!isset($_POST["question_".$i]) || $_POST["question_".$i] == ""){
+      $_SESSION["message"] = "Please do not leave any fields empty";
+      header("Location: ProjectRequirementsRisks.php");
+      exit();
+    }
+
+    $_SESSION["totalWeight"][6] = $_SESSION["totalWeight"][6] + $_POST["question_".$i];
+
+  }
+
+  $_SESSION["message"] = "";
+
+  $total = 0;
+  for($i=0 ; $i<count($_SESSION["totalWeight"]) ; $i++){
+    $total = $total + $_SESSION["totalWeight"][$i];
+  }
+
+  $rating = ($total/320)*100;
+  $complexity = "";
+
+  if($rating<45){
+    $complexity = "Sustaining";
+  }else if($rating>=45 || $rating<=63){
+    $complexity = "Tactical";
+  }else if($rating>=64 || $rating<=82){
+    $complexity = "Evolutionary";
+  }else if($rating>82){
+    $complexity = "Transformational";
+  }
+
+  array_push($_SESSION["newProj"], $complexity);
+  array_push($_SESSION["newProj"], $rating);
+
+  $autoInfo = 'INSERT INTO project
+  (name, owner, financial, duration, mode, complexity, rating) VALUES (:n, :ow, :fn, :dr, :md, :com, :rat)';
+  $stmt = $pdo->prepare($autoInfo);
+  $stmt->execute(array(
+    ':n' => $_SESSION["newProj"][0],
+    ':ow' => $_SESSION["newProj"][1],
+    ':fn' => $_SESSION["newProj"][2],
+    ':dr' => $_SESSION["newProj"][3],
+    ':md' => $_SESSION["newProj"][4],
+    ':com' => $_SESSION["newProj"][5],
+    ':rat' => $_SESSION["newProj"][6])
+  );
+
+  header("Location: registrationDisplay.php");
+  exit();
+
+}
+
+?>
 <html>
 <head>
   <title>Project Complexity and Risk Assessment</title>
 </head>
 <body>
-<?php
-require_once 'pdo.php';
-$stm = $pdo->prepare("select * from question where section_num = 7");
-$stm->execute();
-?>
 <h1>Section 7: Project Requirements Risks (15 Questions)</h1>
-<form method="post" action="result.php">
+<p><?php echo $_SESSION["message"] ?></p>
+<form method="post">
 <ul style="list-style-type:none;">
   <?php while ($question = $stm->fetch(PDO::FETCH_OBJ)){?>
       <li>
@@ -23,8 +90,8 @@ $stm->execute();
            ?>
            <?php while ($choice = $stm2->fetch(PDO::FETCH_OBJ)){?>
              <ul>
-               <input type="radio" name="question_<?php echo $choice->id; ?>"
-               value="<?php echo $choice->id; ?>">
+               <input type="radio" name="question_<?php echo $question->id; ?>"
+               value="<?php echo $choice->weight; ?>">
                <?php echo $choice->choice_content; ?>
              </ul>
            <?php } ?>
@@ -33,7 +100,7 @@ $stm->execute();
   <?php } ?>
   </ul>
   <br>
-  <input type ="submit" value="Submit">
+  <input type ="submit" value="Submit and Finish" name="submit">
 </form>
 <form action="ProjectManagementIntegrationRisks.php">
   <input type="submit" value="Previous Section">
